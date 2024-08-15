c054a2bec48bdfe11ddeee8357b03b1934fb6ca2
leoInStorm <leo.camporesi@hotmail.com>
2024-08-15 16:07:51 -0300

    Projeto totalmente responsivo e aperfeiçoado

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,13 @@
 import './Colaborador.css'
 
-const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
+const Colaborador = ({ nome, imagem, funcao, corDeFundo }) => {
     return (<div className='colaborador'>
         <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
             <img src={imagem} alt={nome}/>
         </div>
         <div className='rodape'>
             <h4>{nome}</h4>
-            <h5>{cargo}</h5>
+            <h5>{funcao}</h5>
         </div>
     </div>)
 }
